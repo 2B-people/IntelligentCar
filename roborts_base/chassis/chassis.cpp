eca8dfc8ba5cb87287ea5d869a95155f8cccd6ef
2B-people <nqq0915@163.com>
2019-07-09 15:34:16 +0800

    回退了rf2o

--- roborts_base/chassis/chassis.cpp
+++ roborts_base/chassis/chassis.cpp
@@ -27,9 +27,9 @@ Chassis::Chassis(std::shared_ptr<roborts_sdk::Handle> handle) : handle_(handle)
 }
 void Chassis::SDK_Init()
 {
-  // handle_->CreateSubscriber<roborts_sdk::cmd_imu_data>(CHASSIS_CMD_SET, CMD_PUSH_IMU_DATA,
-  //                                                      CHASSIS_ADDRESS, MANIFOLD2_ADDRESS,
-  //                                                      std::bind(&Chassis::ImuInfoCallback, this, std::placeholders::_1));
+  handle_->CreateSubscriber<roborts_sdk::cmd_imu_data>(CHASSIS_CMD_SET, CMD_PUSH_IMU_DATA,
+                                                       CHASSIS_ADDRESS, MANIFOLD2_ADDRESS,
+                                                       std::bind(&Chassis::ImuInfoCallback, this, std::placeholders::_1));
   // handle_->CreateSubscriber<roborts_sdk::cmd_uwb_info>(COMPATIBLE_CMD_SET, CMD_PUSH_UWB_INFO,
   //                                                      CHASSIS_ADDRESS, MANIFOLD2_ADDRESS,
   //                                                      std::bind(&Chassis::UWBInfoCallback, this, std::placeholders::_1));
@@ -43,13 +43,13 @@ void Chassis::ROS_Init()
 {
   //ros publisher
   // ros_odom_pub_ = ros_nh_.advertise<nav_msgs::Odometry>("odom", 30);
-  // ros_imu_pub_ = ros_nh_.advertise<sensor_msgs::Imu>("/imu_data", 1);
+  ros_imu_pub_ = ros_nh_.advertise<sensor_msgs::Imu>("/encoder", 1);
   //ros subscriber
-  ros_sub_cmd_chassis_vel_ = ros_nh_.subscribe("cmd_vel", 1, &Chassis::ChassisSpeedCtrlCallback, this);
+  ros_sub_cmd_chassis_vel_ = ros_nh_.subscribe("/car/cmd_vel", 1, &Chassis::ChassisSpeedCtrlCallback, this);
   ros_sub_cmd_chassis_vel_acc_ = ros_nh_.subscribe("cmd_vel_acc", 1, &Chassis::ChassisSpeedAccCtrlCallback, this);
 
   //ros_message_init
-  imu_data_.header.frame_id = "IMU_link";
+  // imu_data_.header.frame_id = "IMU_link";
   // odom_.header.frame_id = "odom";
   // odom_.child_frame_id = "base_link";
 
@@ -60,34 +60,34 @@ void Chassis::ROS_Init()
 }
 void Chassis::ImuInfoCallback(const std::shared_ptr<roborts_sdk::cmd_imu_data> imu_info)
 {
-  Eigen::Vector3d ea0((-0 - imu_info->ph / 1000) * M_PI / 180.0,
-                      0,
-                      0);
-  Eigen::Matrix3d R;
-  R = Eigen::AngleAxisd(ea0[0], ::Eigen::Vector3d::UnitZ()) * Eigen::AngleAxisd(ea0[1], ::Eigen::Vector3d::UnitY()) * Eigen::AngleAxisd(ea0[2], ::Eigen::Vector3d::UnitX());
-  Eigen::Quaterniond q;
-  q = R;
+  // Eigen::Vector3d ea0((-0 - imu_info->ph / 1000) * M_PI / 180.0,
+  //                     0,
+  //                     0);
+  // Eigen::Matrix3d R;
+  // R = Eigen::AngleAxisd(ea0[0], ::Eigen::Vector3d::UnitZ()) * Eigen::AngleAxisd(ea0[1], ::Eigen::Vector3d::UnitY()) * Eigen::AngleAxisd(ea0[2], ::Eigen::Vector3d::UnitX());
+  // Eigen::Quaterniond q;
+  // q = R;
 
-  imu_data_.orientation.w = (double)q.w();
-  imu_data_.orientation.x = (double)q.x();
-  imu_data_.orientation.y = (double)q.y();
-  imu_data_.orientation.z = (double)q.z();
+  // imu_data_.orientation.w = (double)q.w();
+  // imu_data_.orientation.x = (double)q.x();
+  // imu_data_.orientation.y = (double)q.y();
+  // imu_data_.orientation.z = (double)q.z();
 
-  imu_data_.header.stamp = ros::Time::now();
-  imu_data_.angular_velocity.x = (double)imu_info->aax / 1000;
-  imu_data_.angular_velocity.y = (double)imu_info->aay / 1000;
-  imu_data_.angular_velocity.z = (double)imu_info->aaz / 1000;
-  imu_data_.linear_acceleration.x = imu_info->ax * 9.81 / 1000;
-  imu_data_.linear_acceleration.y = imu_info->ay * 9.81 / 1000;
-  imu_data_.linear_acceleration.z = imu_info->az * 9.81 / 1000;
-  ros_imu_pub_.publish(imu_data_);
+  // imu_data_.header.stamp = ros::Time::now();
+  // imu_data_.angular_velocity.x = (double)imu_info->aax / 1000;
+  // imu_data_.angular_velocity.y = (double)imu_info->aay / 1000;
+  // imu_data_.angular_velocity.z = (double)imu_info->aaz / 1000;
+  // imu_data_.linear_acceleration.x = imu_info->ax * 9.81 / 1000;
+  // imu_data_.linear_acceleration.y = imu_info->ay * 9.81 / 1000;
+  // imu_data_.linear_acceleration.z = imu_info->az * 9.81 / 1000;
+  // ros_imu_pub_.publish(imu_data_);
 }
 
 void Chassis::ChassisSpeedCtrlCallback(const geometry_msgs::Twist::ConstPtr &vel)
 {
   roborts_sdk::cmd_chassis_speed chassis_speed;
-  chassis_speed.vx = vel->linear.x * 1000;
-  chassis_speed.vy = vel->linear.y * 1000;
+  chassis_speed.vx = vel->linear.x;
+  chassis_speed.vy = 0.0;
   chassis_speed.vw = vel->angular.z;
   chassis_speed.rotate_x_offset = 0;
   chassis_speed.rotate_y_offset = 0;
@@ -102,7 +102,7 @@ void Chassis::ChassisSpeedAccCtrlCallback(const roborts_msgs::TwistAccel::ConstP
   chassis_spd_acc.vw = vel_acc->twist.angular.z * 1000;
   chassis_spd_acc.ax = vel_acc->accel.linear.x * 1000;
   chassis_spd_acc.ay = 0.0;
-  chassis_spd_acc.wz = vel_acc->accel.angular.z* 1000;
+  chassis_spd_acc.wz = vel_acc->accel.angular.z * 1000;
   chassis_spd_acc.rotate_x_offset = 0;
   chassis_spd_acc.rotate_y_offset = 0;
   chassis_spd_acc_pub_->Publish(chassis_spd_acc);
--- roborts_base/roborts_base_config.h
+++ roborts_base/roborts_base_config.h
@@ -23,7 +23,7 @@ namespace roborts_base{
 
 struct Config {
   void GetParam(ros::NodeHandle *nh) {
-    nh->param<std::string>("serial_port", serial_port, "/dev/car");
+    nh->param<std::string>("serial_port", serial_port, "/dev/ttyUSB0");
   }
   std::string serial_port;
 };
