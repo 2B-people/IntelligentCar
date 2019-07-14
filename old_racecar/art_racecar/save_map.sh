7f51d7778ceff0490e957b9a6faf2b848e68c84b
2b-car <nqq0915@163.com>
2019-07-14 15:55:41 +0800

    #101

--- old_racecar/art_racecar/save_map.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-
-rosrun map_server map_saver -f  ~/racecar/src/art_racecar/map/mymap
--- old_racecar/art_racecar/udev/art_init.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-sudo cp ./car.rules /etc/udev/rules.d
-sudo cp ./laser.rules /etc/udev/rules.d
-sudo cp ./imu.rules /etc/udev/rules.d
-#
-exit 0
